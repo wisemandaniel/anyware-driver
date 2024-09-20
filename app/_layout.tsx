ae2ed272e945202b44e760ad7dd200649a445153
wisemandaniel <ndzodaniel31@gmail.com>
2024-09-20 20:28:01 +0100

    progress

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,8 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
-import { SF_Pro_Display } from "@/assets/fonts";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -26,24 +18,8 @@ export const unstable_settings = {
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-    // ...SF_Pro_Display
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
 
   return (
-    // <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
     <Stack>
       <Stack.Screen name="welcome" options={{ headerShown: false }} />
       <Stack.Screen name="auth" options={{ headerShown: false }} />
@@ -51,6 +27,5 @@ export default function RootLayout() {
       <Stack.Screen name="order" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
-    // </ThemeProvider>
   );
 }
